a7f130e317f9e56750e8ec5a68b83e1c4dfde75c
Daniel-Ugulino <danielugulino46@gmail.com>
2024-09-24 19:59:37 -0300

    feat: minor update

--- taxService/src/main/java/com/infnet/taxService/taxServiceApplication.java
+++ taxService/src/main/java/com/infnet/taxService/taxServiceApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableFeignClients(basePackages = "com.infnet.taxService.client")
+@EnableFeignClients(basePackages = "com.infnet.taxService.Client")
 public class taxServiceApplication {
 
 	public static void main(String[] args) {
